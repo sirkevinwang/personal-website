83dfffaa2cb68c616880d2c0649740c38876cd27
Kevin Wang <kevin.wang11@outlook.com>
2020-12-09 12:57:55 -0500

    Swiper in progress

--- src/components/CoverVideo.js
+++ src/components/CoverVideo.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const CoverVideo = (props) => {
   return (
-    <div className="w-screen h-screen relative">
+    <div className="swiper-slide w-screen h-screen relative">
       <video
         style={{
           objectFit: 'cover',
           width: '100%',
           height: '100%',
         }}
-        src={props.video} autoPlay muted loop playsInline
+        src={props.video} autoPlay muted loop playsInline preload="auto"
         className="opacity-50"
       ></video>
       <div className="h-screen w-screen z-30 absolute top-0 left-0 flex justify-center items-center">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,43 @@
-import Swiper from 'react-id-swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Autoplay, Scrollbar, A11y } from 'swiper';
+
+// Import Swiper styles
 
 import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 
 import CoverVideo from '../components/CoverVideo';
 
 import VidPolaris from '../videos/vid-polaris.mp4';
 import VidArgus from '../videos/vid-argus.mp4';
- 
+
+SwiperCore.use([Navigation, Pagination, Autoplay, Scrollbar, A11y]);
 
 const Home = () => {
   const params = {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
-    }
+    },
+    autoplay: {
+      delay: 5000   
+    },
+    effect: "slide",
+    loop: !0,
+    speed: 500
   }
   return (
-    <div className="vh-100 w-screen items-center justify-center table">
-      {/* <h2 className="table-cell align-middle text-center text-white font-medium text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl"><span className="un">Don't</span> do the right thing.</h2> */}
-      <Swiper {...params} className="">
-          <CoverVideo video={VidPolaris} name="Polaris"></CoverVideo>
-          <CoverVideo video={VidArgus} name="Argus"></CoverVideo>
-      </Swiper>
-    </div>
+    <Swiper {...params}>
+      <SwiperSlide >
+        <div className="h-full flex justify-center items-center">
+          <div><h2 className="text-center text-white font-medium text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl"><span className="un">Don't</span> do the right thing.</h2></div>
+        </div>
+      </SwiperSlide>
+          <SwiperSlide><CoverVideo video={VidPolaris} name="Polaris"></CoverVideo></SwiperSlide>
+          <SwiperSlide><CoverVideo video={VidArgus} name="Argus"></CoverVideo></SwiperSlide>
+    </Swiper>
   );
 };
 

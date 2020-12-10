480f1fb63113ca8f3db40ca36c155025e56217d6
Kevin Wang <kevin.wang11@outlook.com>
2020-12-10 01:19:26 -0500

    updated videos and swiper buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,22 +22,34 @@ const Home = () => {
       prevEl: '.swiper-button-prev'
     },
     autoplay: {
-      delay: 5000   
+      delay: 6e3,   
     },
     effect: "slide",
     loop: !0,
     speed: 500
   }
   return (
+    <>
     <Swiper {...params}>
       <SwiperSlide >
         <div className="h-full flex justify-center items-center">
           <div><h2 className="text-center text-white font-medium text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl"><span className="un">Don't</span> do the right thing.</h2></div>
         </div>
       </SwiperSlide>
-          <SwiperSlide><CoverVideo video={VidPolaris} name="Polaris"></CoverVideo></SwiperSlide>
-          <SwiperSlide><CoverVideo video={VidArgus} name="Argus"></CoverVideo></SwiperSlide>
+          <SwiperSlide>
+          {({ isActive }) => (
+            <CoverVideo video={VidPolaris} name="Polaris"></CoverVideo>
+          )}
+        </SwiperSlide>
+        <SwiperSlide>
+          {({ isActive }) => (
+            <CoverVideo video={VidArgus} name="Argus"></CoverVideo>
+          )}
+        </SwiperSlide>
     </Swiper>
+      <div className="swiper-button-next text-yellow-500 pr-8"></div>
+      <div className="swiper-button-prev text-yellow-500 pl-8"></div>
+    </>
   );
 };
 
---
+++ src/pages/Projects.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Projects = (props) => {
+  return (
+    <div>
+       
+    </div>
+  );
+};
+
+export default Projects;
